ce7a9f624a33d706cb7767acd2ec5d8294df9e87
Muhammad Rizwan <mrizwan1070@gmail.com>
2025-02-06 20:41:35 +0500

    New changes

--- flyaway/front/js/custom.js
+++ flyaway/front/js/custom.js
@@ -1,6 +1,12 @@
 jQuery(document).ready(function () {
-    $(".sortBtn").on("click", function () {
-        $(".sortBtn").removeClass("active");
-        $(this).addClass("active");
-    });
+  $(".sortBtn").on("click", function () {
+    $(".sortBtn").removeClass("active");
+    $(this).addClass("active");
+  });
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
 });
