2d6ff4a36e21eeb487a1a063a4a962242f353e27
Muhammad Rizwan <mrizwan1070@gmail.com>
2025-02-27 17:07:34 +0500

    changes

--- flyaway/front/js/custom.js
+++ flyaway/front/js/custom.js
@@ -58,4 +58,58 @@ jQuery(document).ready(function () {
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
   });
+  $('.airline-slider').owlCarousel({
+    items:10,
+    loop:true,
+    margin:1,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplaySpeed: 2000,
+    slideTransition: '',
+    autoplayHoverPause:true,
+    responsive:{
+        1200 : {
+            items: 7 
+        },
+        800 : {
+            items: 6 
+        },
+        768 : {
+            items: 5 
+        },
+        576 : {
+            items: 4 
+        },
+        480 : {
+            items: 3 
+        },
+        0 : {
+            items: 2
+        }
+    }
+});
+$('.testimonial-slider').owlCarousel({
+    items:10,
+    loop:true,
+    margin:1,
+    autoplay:true,
+    autoplayTimeout:7000,
+    autoplaySpeed: 5000,
+    autoHeight: true,
+    dots: true,
+    nav: true,
+    slideTransition: '',
+    autoplayHoverPause:true,
+    responsive:{
+        1200 : {
+            items: 3 
+        },
+        576 : {
+            items: 2 
+        },
+        0 : {
+            items: 1
+        }
+    }
+});
 });
